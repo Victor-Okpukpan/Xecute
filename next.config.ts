d7d56a78b4ee8fafa14c6c34d8e2e7adb4f6831b
Victor Okpukpan <vokpukpan@gmail.com>
2025-02-06 15:22:38 +0100

    feat: add moralis widget in a jsx file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.stats = "verbose";
+    return config;
+  },
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
